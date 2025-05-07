4bb198d0cb6c12c28d9d5368125e4df23b8ed3a0
Emre0126 <emremutlu135@gmail.com>
2025-05-07 09:50:53 +0300

    ExitApplication.cs

--- ExitApplication.cs
+++ ExitApplication.cs
@@ -4,10 +4,6 @@ public class ExitApplication : MonoBehaviour
 {
     public void ExitGame()
     {
-        // Oyun içindeyken çalýþtýrýldýðýnda log mesajý verir
-        Debug.Log("Uygulama kapatýlýyor...");
-
-        // Uygulamadan çýk
         Application.Quit();
     }
 }
