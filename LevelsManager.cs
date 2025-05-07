f3e6dd727c04339bc4ffd8c600605818272b9e53
Emre0126 <emremutlu135@gmail.com>
2025-05-07 09:53:11 +0300

    LevelsManager.cs

--- LevelsManager.cs
+++ LevelsManager.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject[] panels; // Panel objelerini burada saklayacaðýz
-    public GameObject[] locks;  // Lock objelerini burada saklayacaðýz
+    public GameObject[] panels;
+    public GameObject[] locks;
 
     private void Start()
     {
@@ -12,14 +12,14 @@ public class LevelManager : MonoBehaviour
 
     void UnlockLevels()
     {
-        for (int i = 2; i <= 20; i++) // Level2'den Level20'ye kadar kontrol et
+        for (int i = 2; i <= 20; i++)
         {
-            if (PlayerPrefs.GetInt($"Panel{i}", 1) == 0) // Panel açýlmýþ mý?
+            if (PlayerPrefs.GetInt($"Panel{i}", 1) == 0)
             {
                 panels[i - 2].SetActive(false);
             }
 
-            if (PlayerPrefs.GetInt($"Lock{i}", 1) == 0) // Lock kaldýrýlmýþ mý?
+            if (PlayerPrefs.GetInt($"Lock{i}", 1) == 0)
             {
                 locks[i - 2].SetActive(false);
             }
