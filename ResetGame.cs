744068ddf5d0a5eb95bf159b55d539c8f6ffe53c
Emre0126 <emremutlu135@gmail.com>
2025-05-07 09:56:57 +0300

    ResetGame.cs

--- ResetGame.cs
+++ ResetGame.cs
@@ -4,13 +4,8 @@ public class GameReset : MonoBehaviour
 {
     public void ResetGameData()
     {
-        // Tüm PlayerPrefs verilerini temizle
         PlayerPrefs.DeleteAll();
-
-        // Deðiþiklikleri kaydet
         PlayerPrefs.Save();
-
-        // Ýsteðe baðlý: Oyunu baþtan baþlat
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 }
