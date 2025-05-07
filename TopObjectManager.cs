590e88d1c6c27f44d81ff7e9317201c9623e1946
Emre0126 <emremutlu135@gmail.com>
2025-05-07 10:07:10 +0300

    TopObjectManager.cs

--- TopObjectManager.cs
+++ TopObjectManager.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class CubePositionController : MonoBehaviour
 {
     public List<GameObject> objectsToCheck = new List<GameObject>();
-
     private void Update()
     {
         CheckCubePosition();
     }
-
     private void CheckCubePosition()
     {
         Vector3 cubePosition = transform.position;
-
-        // Listedeki her bir nesnenin x ve z koordinatlarýný kontrol et
         foreach (var obj in objectsToCheck)
         {
             if (obj != null && IsObjectUnderCube(obj))
@@ -24,14 +20,10 @@ public class CubePositionController : MonoBehaviour
             }
         }
     }
-
-    // Nesnenin küp ile ayný x ve z deðerlerine sahip olup olmadýðýný kontrol eder
     private bool IsObjectUnderCube(GameObject obj)
     {
         Vector3 cubePosition = transform.position;
         Vector3 objPosition = obj.transform.position;
-
-        // float karþýlaþtýrmalarýnda Mathf.Approximately kullanmak hassasiyet saðlar
         return Mathf.Approximately(cubePosition.x, objPosition.x) &&
                Mathf.Approximately(cubePosition.z, objPosition.z);
     }
