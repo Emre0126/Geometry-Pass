5acbf6b0ab5deadbe482a82622fe2d555b798768
Emre0126 <emremutlu135@gmail.com>
2025-05-07 09:59:41 +0300

    SceneChanger.cs

--- SceneChanger.cs
+++ SceneChanger.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Hedef sahnenin adýný parametre olarak alýr ve geçiþ yapar.
     public void SwitchScene(string SceneName)
     {
         SceneManager.LoadScene(SceneName);
