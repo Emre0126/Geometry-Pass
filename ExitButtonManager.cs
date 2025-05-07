f796b7a1f61214d31b0182ff6d965142ba7b40da
Emre0126 <emremutlu135@gmail.com>
2025-05-07 09:51:25 +0300

    ExitButtonManager.cs

--- ExitButtonManager.cs
+++ ExitButtonManager.cs
@@ -2,20 +2,15 @@ using UnityEngine;
 
 public class ExitButtonManager : MonoBehaviour
 {
-    public GameObject exitPanel; // Panel
-    public GameObject exitImage; // Image
-    public GameObject button1;   // Ýlk buton
-    public GameObject button2;   // Ýkinci buton
+    public GameObject exitPanel;
+    public GameObject exitImage;
+    public GameObject button1;
+    public GameObject button2;
 
     public void ShowExitUI()
     {
-        // Paneli görünür yap
         exitPanel.SetActive(true);
-
-        // Image'i görünür yap
         exitImage.SetActive(true);
-
-        // Butonlarý görünür yap
         button1.SetActive(true);
         button2.SetActive(true);
     }
