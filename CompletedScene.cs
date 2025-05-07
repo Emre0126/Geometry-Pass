6dbb89c0dd5c138b9f926eee44b15a477aa9eac5
Emre0126 <emremutlu135@gmail.com>
2025-05-07 09:42:29 +0300

    CompletedScene.cs

--- CompletedScene.cs
+++ CompletedScene.cs
@@ -3,19 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    [Tooltip("Geçiþ yapmak istediðin sahnenin adý veya build index'i")]
-    public string sceneName;  // ya da int sceneIndex;
+    
+    public string sceneName;
 
-    // Butonun OnClick() olayýna bu fonksiyonu baðlayacaksýn
+    
     public void ChangeScene()
     {
         if (string.IsNullOrEmpty(sceneName))
         {
-            Debug.LogError("SceneSwitcher: 'sceneName' boþ býrakýlmýþ!");
+            
             return;
         }
         SceneManager.LoadScene(sceneName);
-        // Eðer build index ile yüklenecekse:
-        // SceneManager.LoadScene(sceneIndex);
+       
+        
     }
 }
